8f677b64e245f5c73d0853c8dbb37a5d9cd703ce
hendratay <hendratay96@gmail.com>
2018-11-26 16:24:44 +0700

    get latitude and longitude using google location service

--- app/src/main/java/com/example/tay/nearby/view/activity/MasterActivity.kt
+++ app/src/main/java/com/example/tay/nearby/view/activity/MasterActivity.kt
@@ -1,10 +1,17 @@
 package com.example.tay.nearby.view.activity
 
+import android.app.Activity
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
+import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.net.ConnectivityManager
 import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.view.GravityCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
@@ -16,15 +23,22 @@ import com.example.tay.nearby.model.Place
 import com.example.tay.nearby.view.adapter.PlaceTypeAdapter
 import com.example.tay.nearby.utils.RecyclerViewSnapHelper
 import com.example.tay.nearby.view.adapter.PlaceAdapter
+import com.example.tay.nearby.view.utils.Permission
 import com.example.tay.nearby.viewmodel.PlaceViewModel
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.Task
 import kotlinx.android.synthetic.main.activity_master.*
 
+const val TAG = "MasterActivity"
+const val REQUEST_ACCESS_FINE_LOCATION = 111
+const val REQUEST_CHECK_SETTINGS = 222
 class MasterActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var placeTypeAdapter: PlaceTypeAdapter
@@ -32,15 +46,21 @@ class MasterActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var placeViewModel: PlaceViewModel
     private val listPlaceType: MutableList<String> = ArrayList()
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_master)
         placeViewModel = ViewModelProviders.of(this)[PlaceViewModel::class.java]
-        transparencyBar()
-        setupNavigationDrawer()
-        setupToolbar()
-        setupMap()
-        setupRecyclerView()
+        setupUI()
+        if (hasNetwork()) {
+            createLocationRequest()
+            receiveLocationUpdates()
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+            startLocationUpdates()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -56,6 +76,39 @@ class MasterActivity : AppCompatActivity(), OnMapReadyCallback {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQUEST_CHECK_SETTINGS -> {
+                when(resultCode) {
+                    Activity.RESULT_OK -> startLocationUpdates()
+                    Activity.RESULT_CANCELED -> {
+                        // todo: get saved latitude and longitude
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode) {
+            REQUEST_ACCESS_FINE_LOCATION -> {
+                if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startLocationUpdates()
+                } else {
+                    // todo: get saved latitude and longitude
+                }
+            }
+        }
+    }
+
+    private fun setupUI() {
+        transparencyBar()
+        setupNavigationDrawer()
+        setupToolbar()
+        setupMap()
+        setupRecyclerView()
     }
 
     private fun transparencyBar() {
@@ -87,10 +140,11 @@ class MasterActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    override fun onMapReady(p0: GoogleMap?) {
+    // todo: get latitude and longitude
+    override fun onMapReady(googleMap: GoogleMap?) {
         val sydney = LatLng(-33.852, 151.211)
-        p0?.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        p0?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        googleMap?.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
     private fun setupRecyclerView() {
@@ -108,12 +162,78 @@ class MasterActivity : AppCompatActivity(), OnMapReadyCallback {
         for (placeType in placeTypeArray) listPlaceType.add(placeType)
     }
 
+    // todo: get latitude and longitude
     private fun placeTypeClick(placeType: String) {
         placeViewModel.loadPlace("1.128780, 104.040340", "50000", placeType)
         placeViewModel.getPlace().observe(this,
-                Observer<List<Place>> {
-                    Log.d("GOOGLE PLACES", it.toString())
+                Observer<List<Place>> { listPlace ->
+                    Log.d(TAG, listPlace.toString())
                 })
+    }
+
+    private fun hasNetwork(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetworkInfo
+        return activeNetwork?.isConnectedOrConnecting ?: false
+    }
+
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest().apply {
+            interval = 1000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        val builder = LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest)
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+        task.addOnFailureListener {response ->
+            if (response is ResolvableApiException){
+                try {
+                    response.startResolutionForResult(this, REQUEST_CHECK_SETTINGS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // todo: show toast notify that error when enable location
+                }
+            }
+        }
+    }
+
+    private fun receiveLocationUpdates() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for(location in locationResult.locations) {
+                    // todo: save location.latitude and location.longitude
+                }
+                stopLocationUpdates()
+            }
+        }
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    private fun startLocationUpdates() {
+        if(ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+        } else {
+            requestLocationPermission()
+        }
+    }
+
+    private fun requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_ACCESS_FINE_LOCATION)
+        } else {
+            if(Permission.getPermissionSharedPref(this)) {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_ACCESS_FINE_LOCATION)
+                Permission.savePermissionSharedPref(this, false)
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_ACCESS_FINE_LOCATION)
+                // todo: create snack bar to asking enable location permission
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/tay/nearby/view/utils/Permission.kt
@@ -0,0 +1,27 @@
+package com.example.tay.nearby.view.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
+import com.example.tay.nearby.R
+
+class Permission {
+
+    companion object {
+        private fun getSharedPrefs(context: Context): SharedPreferences {
+            return PreferenceManager.getDefaultSharedPreferences(context)
+        }
+
+        fun savePermissionSharedPref(context: Context, check: Boolean) {
+            val editor =  getSharedPrefs(context).edit()
+            editor.putBoolean(context.getString(R.string.saved_permission), check)
+            editor.apply()
+        }
+
+        fun getPermissionSharedPref(context: Context): Boolean {
+            val prefs = getSharedPrefs(context)
+            return prefs.getBoolean(context.getString(R.string.saved_permission), true)
+        }
+    }
+
+}
