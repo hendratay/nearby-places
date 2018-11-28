4e9a00afae2f357f1a6d5324e74c6570ce646a89
hendratay <hendratay96@gmail.com>
2018-11-28 13:00:46 +0700

    add popup message util

---
+++ app/src/main/java/com/example/tay/nearby/view/utils/PopupMessage.kt
@@ -0,0 +1,34 @@
+package com.example.tay.nearby.view.utils
+
+import android.content.Context
+import android.graphics.Color
+import android.support.design.widget.Snackbar
+import android.view.Gravity
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+
+fun toast(context: Context, message: CharSequence) {
+    val unicodeIcon = String(Character.toChars(0x26A1))
+    val toast = Toast.makeText(context, "$unicodeIcon    ️$message   $unicodeIcon",
+            android.widget.Toast.LENGTH_SHORT)
+    toast.view.apply {
+        setBackgroundColor(Color.BLACK)
+        findViewById<TextView>(android.R.id.message).textSize = 14F
+        findViewById<TextView>(android.R.id.message).setTextColor(Color.WHITE)
+    }
+    val xOffset = (56 * context.resources.displayMetrics.density).toInt()
+    toast.setGravity(Gravity.BOTTOM, 0, xOffset)
+    toast.show()
+}
+
+fun snackBar(view: View, message: CharSequence, action: CharSequence, listener: (View) -> Unit) {
+    val snackBar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
+    snackBar.view.apply {
+        setBackgroundColor(Color.BLACK)
+        findViewById<TextView>(android.support.design.R.id.snackbar_text).setTextColor(Color.LTGRAY)
+    }
+    snackBar.setAction(action, listener)
+    snackBar.setActionTextColor(Color.WHITE)
+    snackBar.show()
+}
